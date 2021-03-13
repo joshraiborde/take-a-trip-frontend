c945ed5757e5b7fcd99a3ffa66dcfc14be0a0399
joshsrai <joshsrai@gmail.com>
2021-03-13 10:27:54 -0500

    created MainContainer

---
+++ src/components/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainContainer = () => {
+  return (
+    <div className="MainContainer">
+      The main container
+    </div>
+  )
+}
+
+export default MainContainer
