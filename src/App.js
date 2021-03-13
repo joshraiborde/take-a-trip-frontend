988422713c3bb06347c18a37d0ede71cfa97ef76
joshsrai <joshsrai@gmail.com>
2021-03-13 10:26:49 -0500

    moved out components to  NavBar and MainContainer from Apps-js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Login from "./components/Login.js"
-import Logout from "./components/Logout.js"
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser.js"
+import NavBar from './components/NavBar.js'
+import MainContainer from './components/MainContainer.js'
 
 class  App extends React.Component {
 
@@ -13,16 +13,15 @@ class  App extends React.Component {
   
   render() {
     return (
-      this.props.currentUser ? <Logout/> : <Login/>
+      <div className="App">
+      <NavBar/>
+      <MainContainer/>
+    </div>
 
     );
   }
 }
 
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-  }
-}
 
-export default connect(mapStateToProps, { getCurrentUser })(App);
+
+export default connect(null, { getCurrentUser })(App);
