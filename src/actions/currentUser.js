a9b9258ff5e3aef6995219c4914543f0ddc025c3
joshsrai <joshsrai@gmail.com>
2021-03-13 20:48:08 -0500

    signup action

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,4 +1,5 @@
 import { resetLoginForm } from "./loginForm.js"
+import { resetSignupForm } from "./signupForm.js"
 import { getMyTrips } from "./myTrips.js"
 
 // synchronous action creators
@@ -18,7 +19,7 @@ export const setCurrentUser = user => {
   
   // asynchronous action creators
   export const login = credentials => {
-    console.log("credentials are", credentials)
+    
     return dispatch => {
       return fetch("http://localhost:3001/api/v1/login", {
         credentials: "include",
@@ -41,6 +42,33 @@ export const setCurrentUser = user => {
         .catch(console.log)
     }
   }
+
+  export const signup = credentials => {
+    return dispatch => {
+      const userInfo = {
+        user: credentials
+      }
+      return fetch("http://localhost:3001/api/v1/signup", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+      })
+        .then(r => r.json())
+        .then(response => {
+          if (response.error) {
+            alert(response.error)
+          } else {
+            dispatch(setCurrentUser(response.data))
+            dispatch(getMyTrips())
+            dispatch(resetSignupForm())
+          }
+        })
+        .catch(console.log)
+    }
+  }
   
 
   export const logout = () => {
@@ -55,7 +83,7 @@ export const setCurrentUser = user => {
 
 
   export const getCurrentUser = () => {
-    console.log("DISPATCHING GET CURRENT USER")
+    
     return dispatch => {
       return fetch("http://localhost:3001/api/v1/get_current_user", {
         credentials: "include",
