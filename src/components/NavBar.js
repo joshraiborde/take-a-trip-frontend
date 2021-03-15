cc58b63636dc7ec9c5e1848e15ff44d903966d38
joshsrai <joshsrai@gmail.com>
2021-03-15 11:38:53 -0400

    added NavLinks for trip routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import Logout from './Logout'
 
 
 
-const NavBar = ({ currentUser }) => {
+const NavBar = ({ currentUser, loggedIn }) => {
     return (
       <div className="NavBar">
-        { currentUser ? <strong>Welcome, {currentUser.attributes.name} from {currentUser.attributes.hometown.city}</strong> : "" }
+      <NavLink exact activeClassName="active" to="/trips"  >My Trips  |  </NavLink>
+      <NavLink exact activeClassName="active" to="/trips/new" >New Trip  |  </NavLink>
+        { loggedIn ? <Logout/> : null}
       </div>
     )
   }
 
 const mapStateToProps = ({ currentUser }) => {
     return {
-      currentUser
+      currentUser,
+      loggedIn: !!currentUser
     }
   }
 
