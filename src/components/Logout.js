363f18ba2a1862ab8a4f77cdf166f2ae70770ed2
joshsrai <joshsrai@gmail.com>
2021-03-15 11:50:27 -0400

    added preventDefault, rerouted Logout with withRouter and history

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from "../actions/currentUser.js"
+import { withRouter } from 'react-router-dom'
 
 
-const Logout = ({ logout }) => {
+const Logout = ({ logout, history }) => {
 
   return (
-    <form onSubmit={logout}>
+    <form onSubmit={(event) => {
+      event.preventDefault()
+      logout()
+      history.push('/')
+    }
+  }>
       <input type="submit" value="Log Out"/>
     </form>
   )
 }
-
-    export default connect(null, { logout } )(Logout)
+// when submitted, send a request to the backend and logs out
+export default withRouter(connect(null, { logout } )(Logout))
