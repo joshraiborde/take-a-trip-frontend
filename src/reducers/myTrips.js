53d8ff42403f976f5efe5cfd239d4c541e62b0d9
joshsrai <joshsrai@gmail.com>
2021-03-16 17:31:12 -0400

    build update and delete trip

--- src/reducers/myTrips.js
+++ src/reducers/myTrips.js
@@ -7,9 +7,10 @@ export default (state = initialState, action) => {
       case "ADD_TRIP":
         return state.concat(action.trip)
       case "UPDATE_TRIP":
-        console.log("in UPDATE TRIP action is", action)
-        return state
-      case "CLEAR_TRIPS":
+        return state.map(trip => trip.id === action.trip.id ? action.trip : trip)
+      case "DELETE_TRIP":
+        return state.filter(trip => trip.id === action.tripId ? false : true)
+        case "CLEAR_TRIPS":
         return initialState
       default:
         return state
